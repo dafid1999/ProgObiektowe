bb7974c56ecdf7c36bf07c804230b4a417efe425
dafid1999 <dafid1999@users.noreply.github.com>
2020-11-05 14:46:03 +0100

    LAB7

---
+++ LAB7/src/pl/imiajd/borawski/NazwanyPunkt.java
@@ -0,0 +1,16 @@
+package pl.imiajd.borawski;
+
+
+public class NazwanyPunkt extends Punkt {
+    private String name;
+    public NazwanyPunkt(int x, int y, String name) {
+        super(x, y);
+        this.name = name;
+    }
+
+    public void show() {
+
+        System.out.println(name + ":<" + x() + ", " + y() + ">");
+    }
+
+}
---
+++ LAB7/src/pl/imiajd/borawski/Punkt.java
@@ -0,0 +1,26 @@
+package pl.imiajd.borawski;
+
+public class Punkt {
+    private int x, y;
+
+    public Punkt(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void show()
+    {
+        System.out.println("<" + x + ", " + y + ">");
+    }
+
+    public int x() {
+
+        return x;
+    }
+
+    public int y() {
+
+        return y;
+    }
+}
